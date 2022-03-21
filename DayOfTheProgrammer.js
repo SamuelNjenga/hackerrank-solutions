3811c85229678ad1ddc50a7e14a3316ac3a2b90f
Samuel Njenga <snjenga704@gmail.com>
2022-03-21 15:13:30 +0300

    feat: add DayOfTheProgrammer.js

---
+++ DayOfTheProgrammer.js
@@ -0,0 +1,17 @@
+function dayOfProgrammer(year) {
+  // Write your code here
+  if (year === 1918) {
+    return "26.09.1918";
+  } else if (
+    (year < 1918 && year % 4 === 0) ||
+    (year > 1918 && (year % 400 === 0 || (year % 4 === 0 && year % 100 !== 0)))
+  ) {
+    return `12.09.${year}`;
+  } else {
+    return `13.09.${year}`;
+  }
+}
+
+console.log(dayOfProgrammer(2017));
+console.log(dayOfProgrammer(2016));
+console.log(dayOfProgrammer(1800));
