25fbf3da594d13800dc3d8141f66bcc5176d909b
Samuel Njenga <snjenga704@gmail.com>
2022-09-19 09:50:59 +0300

    feat: add ExtraLongFactorials.js which returns an extra long factorial result for an integer

---
+++ ExtraLongFactorials.js
@@ -0,0 +1,10 @@
+function extraLongFactorials(n) {
+  const factorial = (int) => {
+    const bigInt = BigInt(int);
+    return bigInt === 0n ? 1n : bigInt * factorial(bigInt - 1n);
+  };
+  console.log(factorial(n).toString());
+}
+
+extraLongFactorials(45);
+extraLongFactorials(25);
